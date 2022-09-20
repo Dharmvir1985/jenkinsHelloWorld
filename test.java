e5931d70e4900add98c87738e13e429c8bd4d4c1
unknown <dharmvirkumar2008@gmail.com>
2022-09-20 19:53:01 +0530

    New files added

---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class Test {
+	public static void main(String[] args) {
+            String s1 = "hello world";
+	    String s2 = "hello world";
+	    String s3 = new String("hello world");
+	    System.out.println(s1 == s2);
+	    System.out.println(s1 == s3);
+	    System.out.println(s1.equals(s2));
+	    System.out.println(s1.equals(s3));
+
+}
+
+}
